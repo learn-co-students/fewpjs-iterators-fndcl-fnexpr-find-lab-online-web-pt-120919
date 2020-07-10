4092ce78a148e567502158a559e76d2e4055e8e2
Anthony <pardo.anthony@gmail.com>
2020-07-09 20:50:10 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objs) {
+  let result = objs.find( obj => obj.result === 'W');
+  return result ? result.year : undefined;
+}
