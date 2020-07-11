1b28758dd2cdde94b0569bcf3e858d0e4c350e5d
Mely07 <damely.tineo07@gmail.com>
2020-07-10 22:34:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(record) {
+  let x = record.find(function(rec) {
+    return rec.result  == "W";
+  })
+  if (x !== undefined){
+      return x.year
+  }
 }
+
