d800957873b78640992bda603f2d711940b0d9c3
Sarah Condon <s.j.condon01@gmail.com>
2020-07-12 20:34:37 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+
+]
+
+const superbowlWin = (arr) => {
+  let year;
+  arr.find(obj => {
+    if (obj["result"] === "W") {
+      year = obj.year;
+    }
+  })
+  return year;
+}
+
