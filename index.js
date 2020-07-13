53f2c701f368009fcee49e45474e699ec3a77e15
gtblackmore <gtblackmore@gmail.com>
2020-07-13 18:55:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(records) {
+  let result = records.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
 }
+
