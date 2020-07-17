04e8c6187f5382ceb3045745047fa85cdeae4ca3
thomas dubiel <github email address>
2020-07-17 08:13:11 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(teamData) {
+  let result;
+  teamData.find(team => {
+    if (team["result"] === "W") {
+      result = team.year;
+    }
+  })
+  return result;
+};
