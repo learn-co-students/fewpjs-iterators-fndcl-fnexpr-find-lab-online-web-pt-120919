87da36fbdb46de8e7992385e1119ab0262bb7669
hnnav <henna.v@icloud.com>
2020-07-17 16:03:08 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(someArray) {
+  let result = someArray.find( someArray => someArray.result === "W" )
+  return !!result ? result.year : undefined
+}
