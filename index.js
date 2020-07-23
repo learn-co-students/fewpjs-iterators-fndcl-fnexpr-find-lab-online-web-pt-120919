d3d7fc7fa15844d334906ca2328871be657568f2
cep104 <candicelizabethpeters@gmail.com>
2020-07-23 18:25:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const testVar = {}
 
+
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(data){
+  
+let win =data.find(function(sport, index){ 
+  if (sport.result === "W"){
+    return true
+  }
+})
+  if(Boolean(win)=== true)
+  {return win.year}
+ 
+
+
+}
+
